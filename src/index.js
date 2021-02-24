bcf1abe0ad6b8c9632317d4d7d07d1e86a91b61e
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-02-25 00:46:16 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+     return (n* -1).toString().split('').reverse().join('');
+  } else return n.toString().split('').reverse().join('');
 }
